c935c911d3040d0ff145278148b755a91d252baa
legoandmars <legoandmars@gmail.com>
2024-03-09 01:59:04 -0800

    scribble

---
+++ Assets/Runtime/Behaviours/ScribbleBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Resat.Behaviours
+{
+    public class ScribbleBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private SpriteRenderer _spriteRenderer;
+
+        [SerializeField]
+        private Sprite _spriteOne;
+
+        [SerializeField]
+        private Sprite _spriteTwo;
+
+        [SerializeField]
+        private float _speed = 1;
+
+        private void Update()
+        {
+            Debug.Log(Time.time % _speed);
+            if (Time.time % _speed > _speed * 0.5f)
+            {
+                _spriteRenderer.sprite = _spriteOne;
+            }
+            else
+            {
+                _spriteRenderer.sprite = _spriteTwo;
+            }
+        }
+    }
+}

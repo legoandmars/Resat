3dc129277cb2fe264067eccef613efc4570409b5
legoandmars <legoandmars@gmail.com>
2024-03-13 01:07:51 -0700

    half cooked more info panel, will do later

---
+++ Assets/Runtime/UI/MoreInfoPanel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Resat.UI
+{
+    public class MoreInfoPanel : MonoBehaviour
+    {
+        [SerializeField]
+        private RawImage? _mainImage;
+        
+        [SerializeField]
+        private RawImage? _colorArrayImage;
+    }
+}

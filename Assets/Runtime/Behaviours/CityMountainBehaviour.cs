b0dd680d3ab801bea6bdd1d349cfae15c053929a
legoandmars <legoandmars@gmail.com>
2024-03-13 17:08:54 -0700

    city jumpscare

---
+++ Assets/Runtime/Behaviours/CityMountainBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Resat.Behaviours
+{
+    public class CityMountainBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _playerTransform = null!;
+        
+        [SerializeField]
+        private List<GameObject> _activeWhenPlayerLow;
+
+        [SerializeField]
+        private List<GameObject> _activeWhenPlayerHigh;
+        
+        private void Update()
+        {
+            foreach (var gameObject in _activeWhenPlayerHigh)
+            {
+                gameObject.SetActive(_playerTransform.position.y > 16f);
+            }
+            foreach (var gameObject in _activeWhenPlayerLow)
+            {
+                gameObject.SetActive(_playerTransform.position.y <= 16f);
+            }
+        }
+    }
+}
